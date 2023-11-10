f1761a51c2485cb8136370c365084119641896dd
FireStreaker2 <michaelccc2019@outlook.com>
2023-11-09 21:14:35 -0800

    timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import { Divider, Flex, Button, Text } from "@chakra-ui/react";
 import { Settings } from ".";
 
 const Timer: React.FC = () => {
+	const [time, setTime] = useState(3600);
+	const [isActive, setIsActive] = useState(false);
+
+	useEffect(() => {
+		let interval: NodeJS.Timeout;
+
+		if (isActive && time > 0) {
+			interval = setInterval(() => {
+				setTime((prevTime) => prevTime - 1);
+			}, 1000);
+		} else if (time === 0) {
+			setIsActive(false);
+		}
+
+		return () => {
+			clearInterval(interval);
+		};
+	}, [isActive, time]);
+
+	const hours = Math.floor(time / 3600);
+	const minutes = Math.floor((time % 3600) / 60);
+	const seconds = time % 60;
+
 	return (
 		<>
 			<Text fontSize="5xl">
-				5:00
+				{String(hours).padStart(2, "0")}: {String(minutes).padStart(2, "0")}:{" "}
+				{String(seconds).padStart(2, "0")}
 			</Text>
 
 			<Divider />
@@ -15,19 +40,18 @@ const Timer: React.FC = () => {
 					<Button
 						bg="blue.300"
 						m="1rem"
-						// onClick={() => {
-						// 	isActive ? setIsActive(false) : setIsActive(true);
-						// }}
+						onClick={() => {
+							isActive ? setIsActive(false) : setIsActive(true);
+						}}
 					>
-						{/* {isActive ? "Stop" : "Start"} */}
-            Start
+						{isActive ? "Stop" : "Start"}
 					</Button>
 					<Button
 						bg="gray.400"
-						// onClick={() => {
-						// 	setTime({ hours: 0, minutes: 0, seconds: 0 });
-						// 	setIsActive(false);
-						// }}
+						onClick={() => {
+							setTime(3600);
+							setIsActive(false);
+						}}
 					>
 						Reset
 					</Button>
